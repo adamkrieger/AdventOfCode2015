31b6f16140fec6b8385df7904e0117254fdbb869
Adam Krieger <akrieger@obsglobal.com>
2015-12-10 20:05:41 -0600

    Day1

---
+++ AdventOfCode/Program.fs
@@ -0,0 +1,13 @@
+﻿open AdventOfCode
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+
+    let day1 = new Day1()
+
+    printfn "%d" day1.solve
+
+    do System.Console.ReadKey() |> ignore
+
+    0 // return an integer exit code
